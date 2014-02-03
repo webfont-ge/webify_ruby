32d48eed53a4b985791aa348862ce63cdf7dae6a
dnatsvlishvili <d.natsvlishvili@gmail.com>
2014-02-03 21:44:34 +0400

    Rewrite

---
+++ lib/webify_rails.rb
@@ -0,0 +1,23 @@
+require 'webify_rails/version'
+require 'webify_rails/convert'
+
+module WebifyRails
+  def self.logger=(log)
+    @logger = log
+  end
+
+  def self.logger
+    return @logger if @logger
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::INFO
+    @logger = logger
+  end
+
+  def self.webify_binary=(bin)
+    @webify_binary = bin
+  end
+
+  def self.webify_binary
+    @webify_binary || 'webify'
+  end
+end
---
+++ lib/webify_rails/version.rb
@@ -0,0 +1,3 @@
+module WebifyRails
+  VERSION = "0.0.1"
+end
