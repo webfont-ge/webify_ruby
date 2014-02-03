81f6a060f374754cc727cf2028734b796e4a2a2c
dnatsvlishvili <d.natsvlishvili@gmail.com>
2014-02-03 22:35:17 +0400

    Initializing convertion

--- lib/webify_rails/convert.rb
+++ lib/webify_rails/convert.rb
@@ -2,8 +2,19 @@ require 'open3'
 
 module WebifyRails
   class Convert
-    def initialize
+    def initialize(file)
+      raise Errno::ENOENT, "The font file '#{file}' does not exist" unless File.exists?(file)
+      @file = file
 
+      command = "#{WebifyRails.webify_binary} #{Shellwords.escape(@file)}"
+      output = Open3.popen3(command) { |stdin, stdout, stderr| stdout.read }
+
+      validity = is_valid? output
+    end
+
+    def is_valid? (output)
+      return false if not output.include? 'Generating' or output.include? 'Failed'
+      true
     end
   end
 end
