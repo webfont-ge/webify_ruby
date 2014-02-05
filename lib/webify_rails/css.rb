32831094d6ffdbf55af6dff25063140a2672973f
dnatsvlishvili <d.natsvlishvili@gmail.com>
2014-02-06 03:16:21 +0400

    Refactors Css class

--- lib/webify_rails/css.rb
+++ lib/webify_rails/css.rb
@@ -15,13 +15,16 @@ module WebifyRails
   CSS
 
   class Css
-    attr_accessor :result
+    attr_reader :result
 
     %w(eot svg woff ttf).each do |ext|
-      define_method('has_' + ext) { true }
+      define_method('has_' + ext) { @has.include? ext.to_sym }
     end
 
-    def initialize(name, file)
+    def initialize(name, file, has)
+      [name, file, has]
+
+      @has = has
       @name = name
       @file = file
 
@@ -30,20 +33,13 @@ module WebifyRails
 
     private
 
-    def get_binding
-      binding
-    end
-
     def make_css
       template = ERB.new TEMPLATE
-
-      result = template.result(get_binding)
-
+      result = template.result(binding)
       (0..3).each { result.gsub!(/\n;\n/m, ";\n") }
-
       @result = result.gsub /^$\n/, ''
     end
   end
 
-  #p Css.new('test','file').result
+  puts Css.new('test', 'file', [:woff, :ttf]).result
 end
